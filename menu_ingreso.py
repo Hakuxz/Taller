be943eb5d9cc2650a158445fc80784845752d58f
Hakuxz <101758285+Hakuxz@users.noreply.github.com>
2022-06-19 18:54:13 -0400

    Add files via upload

--- menu_ingreso.py
+++ menu_ingreso.py
@@ -1,7 +1,9 @@
 import os
 import time
 from coneccion import coneccion 
+from t_jugador import t_jugador
 from t_jugadorDAO import t_jugadorDAO
+#from t_gamemaster import t_gamemaster
 #from t_gamemasterDAO import t_gamemasterDAO
 
 coneccion.getStartConnection()
@@ -10,35 +12,54 @@ jugador_dao = t_jugadorDAO()
 
 ingresoSistema = True
 
-nUsuario = ''
-ccUsuario = ''
-
 def ingresoUsuario():
-    time.sleep(2)
     os.system('cls')
-    print('<<------------------------------<< Menu >>------------------------------>>')
-    print('Ingrese Nombre de Usuario:')
-    nUsuario = input('#: ')
-    print('Ingrese Contraseña de Usuario:')
+    print('<<---------------------<< Menu Ingresar Usuario >>---------------------->>')
+    print('Ingrese su Nombre de Usuario:')
+    nnUsuario = input('#: ')
+    print('Ingrese su Contraseña:')
     ccUsuario = input('#: ')
-    print('<<------------------------------<< Menu >>------------------------------>>')
-    #jugador_dao.comprobarXnombre(nUsuario,ccUsuario)
+    jugador_dao.comprobar_usuario(nnUsuario,ccUsuario)
+
+def crearUsuario():
+    os.system('cls')
+    print('<<-----------------------<< Menu Crear Usuario >>----------------------->>')
+    print('1.- Crear usuario Game Mastre')
+    print('2.- Crear usuario Jugador')
+    seleccion = input('#: ')
+    if seleccion == '1':
+        pass
+    elif seleccion == '2':
+        pass
+    else:
+        print('Valor Ingresado no Valido: '+seleccion)
+
+def creacionUsuario():
+    print('Seleccione un NickName:')
+    nick = input('#: ').upper()
+    print('Cree su Contraseña:')
+    cc = input('#: ')
+    id_j = jugador_dao.buscarID()
+    nuevoPersonaje = t_jugador(id_j,nick,cc,0)
+    jugador_dao.crearJugador(nuevoPersonaje)
 
 # Menu ---------------------------------------->>
 while(ingresoSistema):
     time.sleep(2)
     os.system('cls')
-    print('<<------------------------------<< Menu >>------------------------------>>')
+    print('<<-----------------------<< Menu Crear Usuario >>----------------------->>')
     print('Seleccione que opcion desea ingresando el numero corrrespondiente a esta:')
     print('1.- Ingresar')
-    print('2.- Salir')
+    print('2.- Crear Usuario')
+    print('3.- Salir')
     print('<<------------------------------<< Menu >>------------------------------>>')
     seleccion = input('#: ')
+    #------------------------------------------>>
     if seleccion == '1':
         ingresoUsuario()
-        print('ok')
     elif seleccion == '2':
-        jugador_dao.tablas()
+        crearUsuario()
+    elif seleccion == '3':
         print('Cerrando Sistema, Hasta Pronto')
         ingresoSistema = False
     else:
